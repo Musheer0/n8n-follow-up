a1074990ebfa2aba503ad2526a9bf18f0a63ee8a
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-12-06 19:39:36 +0530

    feat add trpc protected procedue & proxy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import LoginForm from '@/components/auth/login/login-form'
-import RegisterForm from '@/components/auth/register/register-form'
-import AuthForm from '@/components/auth/shared/auth-form'
-import { caller } from '@/trpc/server'
 import React from 'react'
 
-const page = async() => {
-  const d =  await caller.hello({text: 'from server component'})
-
-
+const page = () => {
   return (
     <div>
-       {JSON.stringify(d)}      
-    <AuthForm
-    title='Login'
-    description='login with your account to continue'
-    >
-
-     <RegisterForm/>
-    </AuthForm>
+      
     </div>
   )
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/lib/db";
+import { headers } from "next/headers";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db,{
@@ -11,3 +12,23 @@ export const auth = betterAuth({
     autoSignIn:true
   }
 });
+
+export const requireAuth = async()=>{
+  const session = await auth.api.getSession({
+    headers:await headers()
+  });
+  if(!session?.user){
+    throw new Error("Unauthorized")
+  }
+  return session;
+}
+
+export const requireUnAuth = async()=>{
+  const session = await auth.api.getSession({
+    headers:await headers()
+  });
+  if(session?.user){
+    throw new Error("Already authenticated")
+  }
+  return session;
+}
